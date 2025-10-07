cf8229956b3ccd7133313695ea29b20a1a0a2a1b
Teo <Jokinenteo26@gmail.com>
2025-10-07 18:20:06 +0300

    add hitbox and rigidbody

--- Assets/Scripts/AICar.cs
+++ Assets/Scripts/AICar.cs
@@ -8,40 +8,26 @@ public class AICar : MonoBehaviour
 
     public float speed = 10f;
 
-    public float turnSpeed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float rotationSpeed = 5f;
+    
+    // Update is called once per frame
+    void Update()
     {
-        Transform targetWaypoints = waypoints[currentWaypointIndex];
+        Transform target = waypoints[currentWaypointIndex];
 
-        Vector3 targetXZ = new Vector3(targetWaypoints.position.x, transform.position.y, target.position.z);
-
+        Vector3 targetXZ = new Vector3(target.position.x, transform.position.y, target.position.z);
 
         Vector3 direction = (targetXZ - transform.position).normalized;
 
         Quaternion lookRotation = Quaternion.LookRotation(direction);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Transform target = waypoints[currentWaypointIndex]
-
-        Vector3 targetXZ = new Vector3(target.position.x, transform.position.y, target.position)
-
-        Vector3 direction = (targetXZ - transform.position).normalized;
-
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed *);
 
         transform.Translate(direction * speed * Time.deltaTime);
 
-        if (math.abs(transform.position, target.position) < 2f)
+        if (Vector3.Distance(transform.position, target.position) < 2f)
         {
-            currentWaypointIndex += 1;
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
     }
 }
